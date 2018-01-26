c2c99ce1e6cd3d9bc32724373a0842b9a0a90e26
Bruno Lima <bgcl@ic.ufal.br>
2018-01-26 14:04:25 -0200

    Edited window size and presizing options

--- train_hog_detection.py
+++ train_hog_detection.py
@@ -4,7 +4,6 @@ Created on Wed Jan 24 19:37:23 2018
 
 @author: brunolima
 """
-
 from imutils import paths
 import imutils
 import argparse
@@ -16,28 +15,19 @@ from skimage import feature
 from sliding_window import sliding_window
 from image_pyramid import pyramid
 
-
-#%%
-
 # construct the argument parse and parse command line arguments
 ap = argparse.ArgumentParser()
 ap.add_argument("-d", "--training", required=True, help="Path to the logos training dataset")
 ap.add_argument("-t", "--test", required=True, help="Path to the test dataset")
 args = vars(ap.parse_args())
-
-#%%
-
-#args = {}
-#args["training"] = 'train'
-#args["test"] = 'test'
-
-#%% 
  
 # initialize the data matrix and labels
 print ("[INFO] extracting features...")
-data = []
-labels = []
-(winW, winH) = (200, 100)
+
+data = []   # Feature vectors
+labels = [] # Label for each feature vector
+(winW, winH) = (200, 100) # Window size for images
+min_loss = 10.0 # Min loss so that a result is considered valid
 
 # loop over the image paths in the training set
 for imagePath in paths.list_images(args["training"]):
@@ -75,42 +65,50 @@ model = KNeighborsClassifier(n_neighbors=1)
 model.fit(data, labels)
 print "[INFO] training complete!"
 
-#%%
-
 print "[INFO] evaluating..."
 cv2.namedWindow('MyWindow')
 for imagePath in paths.list_images(args["test"]):
     # load the test image, convert it to grayscale, and resize it to
     # the canonical size
     image_original = cv2.imread(imagePath)
-    image_pre_sized = cv2.resize(image_original, (400, 200))
-
+    # Make image size pre determined
+    image_pre_sized = cv2.resize(image_original, (500, 250))
+    # List of detections that will be sorted
     detected = []
 
-    for image_resized in pyramid(image_pre_sized, downscale=1.2):
+    print "Analyzing {}...".format(imagePath)
+    for image_resized in pyramid(image_pre_sized, downscale=1.2, minSize=(winW, winH)):
         for (x, y, image) in sliding_window(image_resized, stepSize=16, windowSize=(winW, winH) ):
+            # convert to grayscale
             gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+            # discard if window does not match prescpecified dimensions.
+            # This happens when window reaches end of image wither vertically or horizontally.
             if gray.shape[0] != winH or gray.shape[1] != winW:
                 continue
 
 
-            #logo = cv2.resize(gray, (200, 100))     
             # extract Histogram of Oriented Gradients from the test image and
             # predict the make of the car
             (H, hogImage) = feature.hog(gray, orientations=9, pixels_per_cell=(10, 10),
                  cells_per_block=(2, 2), transform_sqrt=True, visualise=True)
-            	
+            
+            # Make H a line vector
             X = H.reshape(1, -1)
+            # Predict loss from 1 nearest neighbor
             loss, _ = model.kneighbors(X, 1, return_distance=True)
+            # Predict label from it
             pred = model.predict(X)[0]
             print('{} of distance from class {}'.format(loss, pred))
             
-            if loss < 5.0:
+            # If loss is acceptable, add to detected vector
+            if loss < min_loss:
                 detected.append([x, y, loss, gray, pred])
     
-    sorted_detection = sorted( detected, key=lambda x: x[2] )
-    print('Best detection loss: {} as being from class {}'.format(sorted_detection[0][2], sorted_detection[0][4]))
-    cv2.imshow('MyWindow', sorted_detection[0][3])
+    # Find minimum loss between 
+    best_candidate = min( detected, key=lambda x: x[2] )
+    print('Best detection from {} got loss {} as being from class {}'.format(imagePath, best_candidate[2], best_candidate[4]))
+    cv2.imshow('MyWindow', best_candidate[3])
     key = cv2.waitKey(0)
     if ( key == ord('q') ):
         break
